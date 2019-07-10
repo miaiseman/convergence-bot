07d4b673496a48b3ce2c3d0b4caa128d4c3dcaef
mia <mia.iseman@gmail.com>
2019-07-10 16:20:09 -0700

    remove old html file

--- convergence/webapp/app.py
+++ convergence/webapp/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__, static_url_path="")
 @app.route('/') 
 def index():
     """Return the main page."""
-    return render_template('/index.html')
+    return render_template('/convergence.html')
 
 
 @app.route('/play_round', methods=['GET', 'POST'])
